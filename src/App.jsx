68bd233bded60236fc561690457abcd993447902
stu <stumac1000@yahoo.com>
2025-06-09 23:55:06 -0700

    Set up initial routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
 
-function App() {
-  return "Howdy";
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
-
-export default App;
