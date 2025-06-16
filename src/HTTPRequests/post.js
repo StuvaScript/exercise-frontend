35c149ca846b9d914e2aa788aaf04880c198f488
stu <stumac1000@yahoo.com>
2025-06-16 15:17:26 -0700

    Create 'add exercise' functionality

--- src/HTTPRequests/post.js
+++ src/HTTPRequests/post.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-const postRequest = (url, body) => {
+const postRequest = (url, body, token) => {
   return axios.post(url, body, {
     headers: {
       "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
     },
   });
 };
--- src/pages/AddEditExercise.jsx
+++ src/pages/AddEditExercise.jsx
@@ -1,18 +1,137 @@
-import { Link, useLocation } from "react-router-dom";
+import { useContext, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { postRequest } from "../HTTPRequests";
+import AuthContext from "../context/AuthContext";
 
 export default function AddEditExercise() {
+  const [formInputs, setFormInputs] = useState({
+    name: "",
+    reps: "",
+    sets: "",
+    measurement: "",
+    measurementUnit: "",
+  });
+
+  const [error, setError] = useState({
+    value: false,
+    msg: "",
+  });
+
+  const navigate = useNavigate();
   const location = useLocation();
   const data = location.state;
+  const { token } = useContext(AuthContext);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormInputs((values) => ({ ...values, [name]: value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    setError({
+      value: false,
+      msg: "",
+    });
+
+    const url = "/api/v1/exercises";
+    const body = {
+      name: formInputs.name,
+      reps: formInputs.reps,
+      sets: formInputs.sets,
+      measurement: formInputs.measurement,
+      measurementUnit: formInputs.name,
+    };
+
+    try {
+      const response = await postRequest(url, body, token);
+
+      console.log("response:", response);
+      if (response.status === 201) {
+        setFormInputs({
+          name: "",
+          reps: "",
+          sets: "",
+          measurement: "",
+          measurementUnit: "",
+        });
+
+        navigate("/dashboard");
+      }
+    } catch (err) {
+      setError({ value: true, msg: `${err.response.data.msg}` });
+    }
+  };
+
   return (
     <>
       <h2>I am the AddEditExercise page</h2>
       <p>{data === "edit" ? "Edit form" : "Add form"}</p>
-      <Link to={"/dashboard"}>
-        <button>AddEdit Exercise</button>
-      </Link>
-      <Link to="/dashboard">
-        <button>Cancel</button>
-      </Link>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">name: </label>
+        <input
+          name="name"
+          id="name"
+          value={formInputs.name}
+          onChange={handleChange}
+          required
+        />
+        <br />
+
+        <label htmlFor="sets">sets: </label>
+        <input
+          name="sets"
+          id="sets"
+          type="number"
+          min={0}
+          value={formInputs.sets}
+          onChange={handleChange}
+        />
+        <br />
+
+        <label htmlFor="reps">reps: </label>
+        <input
+          name="reps"
+          id="reps"
+          type="number"
+          min={0}
+          value={formInputs.reps}
+          onChange={handleChange}
+        />
+        <br />
+
+        <label htmlFor="measurement">measurement: </label>
+        <input
+          name="measurement"
+          id="measurement"
+          type="number"
+          min={0}
+          value={formInputs.measurement}
+          onChange={handleChange}
+          required
+        />
+        <br />
+
+        <label htmlFor="measurementUnit">measurement Unit: </label>
+        <input
+          name="measurementUnit"
+          id="measurementUnit"
+          value={formInputs.measurementUnit}
+          onChange={handleChange}
+          required
+        />
+        <br />
+
+        <button type="submit">
+          {data === "edit" ? "Edit exercise" : "Add exercise"}
+        </button>
+
+        <Link to="/dashboard">
+          <button type="button">Cancel</button>
+        </Link>
+      </form>
+      {error.value && <p>{error.msg}</p>}
     </>
   );
 }
