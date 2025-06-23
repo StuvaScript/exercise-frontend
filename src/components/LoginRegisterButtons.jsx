8284ffe64ae560ed753d099563d05b9d2b6c54a5
stu <stumac1000@yahoo.com>
2025-06-22 23:26:38 -0700

    Fix responsive design

--- src/components/LoginRegisterButtons.jsx
+++ src/components/LoginRegisterButtons.jsx
@@ -1,9 +1,7 @@
 import { Link } from "react-router-dom";
-import styles from "./LoginRegisterButtons.module.css";
-
 export default function LoginRegisterButtons() {
   return (
-    <div className={styles["button-group"]}>
+    <>
       <Link to="/login">
         <button>Log In</button>
       </Link>
@@ -11,6 +9,6 @@ export default function LoginRegisterButtons() {
       <Link to="/register">
         <button>Register</button>
       </Link>
-    </div>
+    </>
   );
 }
